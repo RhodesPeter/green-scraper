7dea3b8606bc4e872193b1fb05e1afa95bc93485
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-11-22 18:57:49 +0000

    Modularise urls

---
+++ scraper/urls.js
@@ -0,0 +1,13 @@
+const urls = [
+  'https://www.theguardian.com/uk/environment',
+  'https://www.theguardian.com/uk/technology',
+  'https://www.theguardian.com/science',
+  'https://www.theguardian.com/environment/climate-change',
+  'https://www.bbc.co.uk/news/technology',
+  'https://www.bbc.co.uk/news/science_and_environment',
+  'https://www.dailymail.co.uk/home/index.html',
+  'https://www.independent.co.uk/environment',
+  'https://www.independent.co.uk/environment/climate-change'
+];
+
+module.exports = urls;
